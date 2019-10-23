a8a8d5b07cc875d412db57259c1e411487969417
guanxianghui001 <guanxianghui001>
2019-10-23 18:45:51 +0800

    增加配置文件

--- Email/__init__.py
+++ Email/__init__.py
@@ -1,5 +1,5 @@
 import configparser
 conf = configparser.ConfigParser()
-conf.read(r"C:\Users\guanxianghui001\Desktop\学习\testDemo\Email\config.iniconfig.ini")
+conf.read(r"C:\Users\guanxianghui001\Desktop\学习\testDemo\Email\config.ini")
 sender=conf.get("email","sender")
 print(sender)
